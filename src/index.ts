d6dbae7561519165558c093b1d949132d049bdc6
Valentin Hăloiu <valentin.haloiu@gmail.com>
2024-12-02 01:05:19 +0000

    Add basic worker implementation that reads from R2 bucket
    Sample code taken from: https://developers.cloudflare.com/r2/examples/cache-api/

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env, context): Promise<Response> {
+		const url = new URL(request.url);
+
+		// Construct the cache key from the cache URL
+		const cacheKey = new Request(url.toString(), request);
+		const cache = caches.default;
+
+		// Check whether the value is already available in the cache
+		// if not, you will need to fetch it from R2, and store it in the cache
+		// for future access
+		let response = await cache.match(cacheKey);
+		console.log({ event: response ? 'cache_hit' : 'cache_miss', url: request.url });
+		if (response) {
+			return response;
+		}
+
+		// If not in cache, get it from R2
+		const objectKey = url.pathname.slice(1);
+		const object = await env.REPO_BUCKET.get(objectKey);
+		if (object === null) {
+			return new Response('Object Not Found', { status: 404 });
+		}
+
+		// Set the appropriate object headers
+		const headers = new Headers();
+		object.writeHttpMetadata(headers);
+		headers.set('etag', object.httpEtag);
+
+		// Cache API respects Cache-Control headers. Setting s-max-age to 10
+		// will limit the response to be in cache for 10 seconds max
+		// Any changes made to the response here will be reflected in the cached value
+		headers.append('Cache-Control', 's-maxage=10');
+
+		response = new Response(object.body, { headers });
+
+		// Store the fetched response as cacheKey
+		// Use waitUntil so you can return the response without blocking on
+		// writing to cache
+		context.waitUntil(cache.put(cacheKey, response.clone()));
+
+		return response;
 	},
 } satisfies ExportedHandler<Env>;
