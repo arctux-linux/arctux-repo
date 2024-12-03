41ce2d6c1807f646d7beeebe022012bb73efbdeb
Valentin Hăloiu <valentin.haloiu@gmail.com>
2024-12-03 01:58:50 +0000

    [worker] refactor: minor code tidying

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,18 @@
 export default {
   async fetch(request, env, context): Promise<Response> {
     const url = new URL(request.url);
-
-    // Construct the cache key from the cache URL
-    const cacheKey = new Request(url.toString(), request);
     const cache = caches.default;
+    const cacheKey = new Request(url.toString(), request);
+    const objectKey = url.pathname.slice(1);
 
-    // Check whether the value is already available in the cache
-    // if not, you will need to fetch it from R2, and store it in the cache
-    // for future access
     let response = await cache.match(cacheKey);
     console.log({ event: response ? 'cache_hit' : 'cache_miss', url: request.url });
     if (response) {
       return response;
     }
 
-    const objectKey = url.pathname.slice(1);
-
-    // No need to query the bucket if the object key does not start with a
-    // known path prefix.
-    if (objectKey === '' || !objectKey.startsWith('aarch64/')) {
-      return new Response('Object Not Found', { status: 404 });
-    }
-
-    // If not in cache, get it from R2
-    const object = await env.REPO_BUCKET.get(objectKey);
-    if (object === null) {
+    const object = await getObjectFor(objectKey, env);
+    if (!object) {
       return new Response('Object Not Found', { status: 404 });
     }
 
@@ -40,3 +27,13 @@ export default {
     return response;
   },
 } satisfies ExportedHandler<Env>;
+
+async function getObjectFor(objectKey: string, env: Env): Promise<R2ObjectBody | null> {
+  // Minor optimization: No need to query the bucket if the object key does not
+  // start with a known path prefix.
+  if (objectKey === '' || !objectKey.startsWith('aarch64/')) {
+    return null;
+  }
+
+  return env.REPO_BUCKET.get(objectKey);
+}
