f640fd970c400121ac9099f554bbc5e19739aa3f
Valentin Hăloiu <valentin.haloiu@gmail.com>
2024-12-06 23:56:15 +0000

    [sync] Only sync explicit repositories

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -7,7 +7,13 @@ pull_from_mirror() {
 }
 
 push_to_bucket() {
-    ssh rsync.net rclone sync --copy-links --progress mirrors/archlinuxarm/aarch64 r2:arctux-repo/aarch64
+    ssh rsync.net rclone sync \
+        --copy-links \
+        --progress \
+        --include='/sync' \
+        --include="/{alarm,aur,core,extra}/**" \
+        mirrors/archlinuxarm/aarch64 \
+        r2:arctux-repo/aarch64
 }
 
 main() {
